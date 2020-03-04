af50738686893aba7d498f297a5d1c74d543b931
Aamir Husain <aamir.husain@verizonwireless.com>
2020-03-04 18:30:48 -0500

    implement test sine function

--- jackal_base/src/jackal_hardware.cpp
+++ jackal_base/src/jackal_hardware.cpp
@@ -91,7 +91,7 @@ void JackalHardware::publishDriveFromController()
 {
   if (cmd_drive_pub_.trylock())
   {
-    float num = sin(ros::Time::now().toSec()/1000000.0);
+    float num = sin(ros::Time::now().toSec()/100.0);
     std::cout << num << std::endl;
 
     cmd_drive_pub_.msg_.mode = jackal_msgs::Drive::MODE_VELOCITY;
