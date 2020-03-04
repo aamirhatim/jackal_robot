77ef49d7e3918a0d3826575220bc6236408a176c
Aamir Husain <aamir.husain@verizonwireless.com>
2020-03-04 18:33:16 -0500

    tweak sine function

--- jackal_base/src/jackal_hardware.cpp
+++ jackal_base/src/jackal_hardware.cpp
@@ -91,7 +91,7 @@ void JackalHardware::publishDriveFromController()
 {
   if (cmd_drive_pub_.trylock())
   {
-    float num = sin(ros::Time::now().toSec()/100.0);
+    float num = sin(ros::Time::now().toSec()/180.0 * 3.14);
     std::cout << num << std::endl;
 
     cmd_drive_pub_.msg_.mode = jackal_msgs::Drive::MODE_VELOCITY;
