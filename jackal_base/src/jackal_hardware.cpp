caaa191557823520f8df91bef199ba2772f14015
Aamir Husain <aamir.husain@verizonwireless.com>
2020-03-05 10:48:16 -0500

    remove comments

--- jackal_base/src/jackal_hardware.cpp
+++ jackal_base/src/jackal_hardware.cpp
@@ -110,12 +110,12 @@ void JackalHardware::publishDriveFromController()
       // Get current velocities
       double left_vel = joints_[0].velocity / 10.0;
       double right_vel = joints_[1].velocity / 10.0;
-      std::cout << left_vel << std::endl;
+      // std::cout << left_vel << std::endl;
 
       // Calculate deceleration
-      double v_left = std::max(0.0, fabs(left_vel) - 0.03);
-      std::cout << v_left << std::endl;
-      double v_right = std::max(0.0, fabs(right_vel) - 0.03);
+      double v_left = std::max(0.0, fabs(left_vel) - 0.02);
+      // std::cout << v_left << std::endl;
+      double v_right = std::max(0.0, fabs(right_vel) - 0.02);
       if (left_vel < 0.0)
       {
         v_left = -v_left;
@@ -127,7 +127,7 @@ void JackalHardware::publishDriveFromController()
 
       // Create Drive message
       // double v = sin(time_now.toSec());
-      std::cout << v_left << std::endl << std::endl;
+      // std::cout << v_left << std::endl << std::endl;
       cmd_drive_pub_.msg_.mode = jackal_msgs::Drive::MODE_VELOCITY;
       cmd_drive_pub_.msg_.drivers[jackal_msgs::Drive::LEFT] = v_left * 10;
       cmd_drive_pub_.msg_.drivers[jackal_msgs::Drive::RIGHT] = v_left * 10;
